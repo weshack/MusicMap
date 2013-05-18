b798f2b3e23ce358e2acce29d27afb6fe2de47fc
Matt Adelman <madelman@wesleyan.edu>
2013-05-18 12:40:32 -0400

    added songs scaffold

--- music_map/config/routes.rb
+++ music_map/config/routes.rb
@@ -1,4 +1,6 @@
 MusicMap::Application.routes.draw do
+  get "home/index"
+
   resources :songs
 
 
@@ -51,7 +53,7 @@ MusicMap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
