49bf915da86565bdcefbd89d2bdf30500182e80c
Julian Applebaum <julianapplebaum@gmail.com>
2013-05-18 16:33:33 -0400

    Basic autocomplete on top of map marker.

--- static/js/mainview.js
+++ static/js/mainview.js
@@ -1,35 +1,58 @@
-YUI().use('node', function(Y) {
-  SIDEBAR_WIDTH = 300;
+YUI().use('node', 'autocomplete', function(Y) {
+  var WES_COORDS = new google.maps.LatLng(41.555577, -72.657437);
+  var INIT_ZOOM = 17;
+  var SEARCH_TPL = "<div class='songsearch'>" +
+                      "<div class='instructions'>Search for a song to " +
+                      "tag at this location" +
+                      "</div>" +
+                      "<input class='searchbox' placeholder='Search...'>"
+                    "</div>";
+
   google.maps.visualRefresh = true;
 
-  // function updateLayout() {
-  //   width = Y.one('body').get('winWidth');
-  //   height = Y.one('body').get('winHeight');
-  //   Y.one('#map-canvas').setStyle('width', width-SIDEBAR_WIDTH-2)
-  // }
-
-  function placeMarker(position, map) {
+  function tagSong(position, map) {
     var marker = new google.maps.Marker({
       position: position,
       map: map
     });
     map.panTo(position);
+    searchWindow = new google.maps.InfoWindow({
+      content: SEARCH_TPL,
+      position: position,
+      maxWidth: 500
+    });
+    searchWindow.open(map);
+    // TODO: This is hacky. Why doesn't the DOM node exist immediately
+    // after the InfoWindow is created?
+    Y.later(200, Y, function() {
+      var ac = new Y.AutoComplete({
+        inputNode: Y.one('.songsearch .searchbox'),
+        render: false,
+        source: ["We Found Love", "Can't Stop", "Ay Bay Bay"]
+      });
+      ac.render();
+      // TODO: This is super hacky. Try to find a better way to make
+      // the autocomplete results drop below the InfoWindow.
+      Y.one('.songsearch').get('parentNode')
+                          .get('parentNode')
+                          .setStyle('overflow', 'visible');
+    });
+
+
   }
 
   function initialize() {
     var mapOptions = {
-      center: new google.maps.LatLng(41.555577, -72.657437),
-      zoom: 8,
+      center: WES_COORDS,
+      zoom: INIT_ZOOM,
       mapTypeId: google.maps.MapTypeId.ROADMAP
     };
 
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
     google.maps.event.addListener(map, 'click', function(e) {
-      placeMarker(e.latLng, map);
+      tagSong(e.latLng, map);
     });
   }
 
-  google.maps.event.addDomListener(window, 'load', initialize);
-  // Y.one('window').on('resize', updateLayout);
-  // updateLayout();
+  Y.on('domready', initialize);
 });
