f6fb9c884c540a8082789daeb2ff6006e862f190
Matt Adelman <madelman@wesleyan.edu>
2013-05-19 01:00:19 -0400

    added facebook login

--- music_map/config/initializers/omniauth.rb
+++ music_map/config/initializers/omniauth.rb
@@ -1,10 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.development?
-    OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
-    provider :facebook, '75965900421', '7f3051162139e3fec21fe450ae988b64'
-  else
-    provider :facebook, '75965900421', '7f3051162139e3fec21fe450ae988b64'
-  end
+  provider :facebook, ENV['75965900421'], ENV['7f3051162139e3fec21fe450ae988b64']
 end
--- music_map/db/schema.rb
+++ music_map/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130519031308) do
+ActiveRecord::Schema.define(:version => 20130519044320) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -44,9 +44,13 @@ ActiveRecord::Schema.define(:version => 20130519031308) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "oauth_token"
+    t.datetime "oauth_expires_at"
+    t.datetime "created_at",       :null => false
+    t.datetime "updated_at",       :null => false
   end
 
 end
