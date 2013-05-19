7fcd2cb4ddc2836bf659b3bd5ca51afe978192e9
Matt Adelman <madelman@wesleyan.edu>
2013-05-19 13:21:47 -0400

    trying to add mobile site

--- music_map/app/controllers/songs_controller.rb
+++ music_map/app/controllers/songs_controller.rb
@@ -130,7 +130,7 @@ class SongsController < ApplicationController
   # GET /close_songs/:coord.json
   def show_close_songs
     # finds all songs tagged within a .5 mile radius
-    radius = 0.5
+    radius = 0.1
     lat = params[:lat].to_f #lat_lng_list[0].to_f
     lng = params[:lng].to_f #lat_lng_list[1].to_f
     location = [lat, lng]
@@ -140,7 +140,7 @@ class SongsController < ApplicationController
       format.html { redirect_to root_path }
       format.json { render :json => @songs }
     end
-    @songs = Song.near(location, radius)
+    #@songs = Song.near(location, radius)
   end
 
   private
@@ -152,10 +152,10 @@ class SongsController < ApplicationController
 
     # given a latitude(lat) and longitude(lng), return a list of 
 
-  def set_geolocation
-    session[:location] = {:latitude => params[:latitude],
-                          :longitude => params[:longitude] }
-  end
+    def set_geolocation
+      session[:location] = {:latitude => params[:latitude],
+                            :longitude => params[:longitude] }
+    end
     # given a latitude(lat) and longitude(lng), return a list of
     # songs that were tagged near that
     def surrounding_songs(lat, long)
@@ -164,37 +164,37 @@ class SongsController < ApplicationController
       @close_songs = Song.where(distance([:latitude, :longitude], [lat, long]) < radius )
     end
 
-  def list_query(query)
-    client = Sevendigital::Client.new
-    ret_list = []
-    if query.length < 7
-      for song in query
-        song_id = song.id
-        details = client.track.get_details(song_id)
-        artist = details.artist.name
-        title = details.title
-        album = details.release.title
-        stream_url = details.preview_url
-        art_url = details.release.image(100)
-        ret_list.push( { :song_id => song_id, :artist => artist,
-                         :album => album, :song => title,
-                         :stream_url => stream_url, :art_url => art_url } )
+    def list_query(query)
+      client = Sevendigital::Client.new
+      ret_list = []
+      if query.length < 7
+        for song in query
+          song_id = song.id
+          details = client.track.get_details(song_id)
+          artist = details.artist.name
+          title = details.title
+          album = details.release.title
+          stream_url = details.preview_url
+          art_url = details.release.image(100)
+          ret_list.push( { :song_id => song_id, :artist => artist,
+                          :album => album, :song => title,
+                          :stream_url => stream_url, :art_url => art_url } )
+        end
+      else
+        for i in 0..6
+          song_id = query[i].id
+          details = client.track.get_details(song_id)
+          artist = details.artist.name
+          title = details.title
+          album = details.release.title
+          stream_url = details.preview_url
+          art_url = details.release.image(100)
+          ret_list.push( { :song_id => song_id, :artist => artist,
+                          :album => album, :song => title,
+                          :stream_url => stream_url, :art_url => art_url } )
+        end
       end
-    else
-      for i in 0..6
-        song_id = query[i].id
-        details = client.track.get_details(song_id)
-        artist = details.artist.name
-        title = details.title
-        album = details.release.title
-        stream_url = details.preview_url
-        art_url = details.release.image(100)
-        ret_list.push( { :song_id => song_id, :artist => artist,
-                         :album => album, :song => title,
-                         :stream_url => stream_url, :art_url => art_url } )
-      end
+      return ret_list
     end
-    return ret_list
-  end
 
 end
