10a6dfe50897fabc83f01535c6de57aa34e7ae3c
Julian Applebaum <julianapplebaum@gmail.com>
2013-05-19 19:31:27 -0400

    Trying a hack to fix gmaps pins

--- music_map/app/assets/javascripts/mainview.js
+++ music_map/app/assets/javascripts/mainview.js
@@ -1,6 +1,9 @@
 YUI().use('node', 'autocomplete',
   'stylesheet', 'json', 'io', 'common',
 function(Y) {
+  var img = new Image();
+  img.src = "https://mts.googleapis.com/vt/icon/name=icons/spotlight/spotlight-poi.png&scale=1";
+
   var WES_COORDS = new google.maps.LatLng(41.555577, -72.657437);
   var INIT_ZOOM = 17;
   var SEARCH_TPL = "<div class='songsearch'>" +
@@ -75,7 +78,6 @@ function(Y) {
     Y.io('/close_songs/' + latitude + '/' + longitude + '/' + radius +
          "/song.json");
     }
-
 
   function postTag(e, latLng, infoWindow) {
     var songRec = e.result.raw,
@@ -223,6 +225,7 @@ function(Y) {
             map: map,
             animation: google.maps.Animation.DROP,
         });
+        console.log(marker);
         google.maps.event.addListener(marker, 'click', makeMarkerCallback(songTag, latLng));
       }
     });
