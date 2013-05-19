7baa20d1613c2b6cf7688670c8657bbd3fdbacef
Aaron Rosen <azrosen92@gmail.com>
2013-05-19 01:08:12 -0400

    fixed facebook login

--- music_map/config/initializers/omniauth.rb
+++ music_map/config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['75965900421'], ENV['7f3051162139e3fec21fe450ae988b64']
+  provider :facebook, '75965900421', '7f3051162139e3fec21fe450ae988b64'
 end
