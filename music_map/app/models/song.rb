920f6f0b70e9b8a7c82f8b77358803a4fabd71c2
Matt Adelman <madelman@wesleyan.edu>
2013-05-18 21:48:50 -0400

    working again

--- music_map/app/models/song.rb
+++ music_map/app/models/song.rb
@@ -1,11 +1,6 @@
 class Song < ActiveRecord::Base
   reverse_geocoded_by :latitude, :longitude
   after_validation :reverse_geocode
-<<<<<<< HEAD
-  attr_accessible :song_id, :user, :latitude, :longitude
-=======
   attr_accessible :song_id, :user, :latitude, :longitude, :artist, :album, 
                   :stream_url, :art_url, :song
->>>>>>> 0fe7ff8b1448a70183a62fec3094dbbdfe9ee8b5
-
 end
