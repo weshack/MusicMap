723752d5f34aa370512bd13f064a06550fcb4e82
Julian Applebaum <julianapplebaum@gmail.com>
2013-05-19 06:51:16 -0400

    Much higher functionality for song tagging. Show album art, song, address, and a working play/pause button.

--- music_map/app/assets/javascripts/mainview.js
+++ music_map/app/assets/javascripts/mainview.js
@@ -1,4 +1,4 @@
-YUI().use('node', 'autocomplete', 'gallery-player', 'slider',
+YUI().use('node', 'autocomplete', 'gallery-player',
   'stylesheet', 'json', 'io',
 function(Y) {
   var WES_COORDS = new google.maps.LatLng(41.555577, -72.657437);
@@ -15,11 +15,52 @@ function(Y) {
                         "{artist}<br>{album}" +
                       "</div>" +
                     "</div>";
+  var SONG_TAG_TPL = "<div class='song'>" +
+                       "<img class='album-art' src={art_url}>" +
+                       "<div class='song-info'>" +
+                         "<div class='song-name'>{song}</div>" +
+                         "{artist}<br>{album}<br>" +
+                         "<div class='address'>{address}</div>" +
+                         "<a class='playpause' href='{stream_url}'>Play/Pause</a>"
+                       "</div>" +
+                     "</div>";
   var windowWidth, windowHeight;
   var map;
   var responsiveStyle = new Y.StyleSheet();
   var curSearchWindow = null;
+  var curMarkerDisplay = null;
+  var audioPlayer = AudioPlayer();
   google.maps.visualRefresh = true;
+
+  function AudioPlayer() {
+    var player = Y.Node.create("<audio></audio>").getDOMNode();
+    var isPlaying = false;
+    Y.one("body").append(player);
+    return {
+      setSource: function(src) {
+        player.src = src;
+      },
+
+      play: function() {
+        player.play();
+      },
+
+      pause: function() {
+        player.pause();
+      },
+
+      toggle: function() {
+        if (isPlaying) {
+          console.log("PAUSE");
+          this.pause();
+        }
+        else {
+          this.play();
+        }
+        isPlaying = !isPlaying;
+      }
+    }
+  }
 
   function resizeResponse() {
     var canvasHeight = parseInt(Y.one('#map-canvas').getComputedStyle('height'));
@@ -124,62 +165,92 @@ function(Y) {
     curSearchWindow.open(map);
   }
 
+  function placeRadius(position, map) {
+     var circleOptions = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0.6,
+      strokeWeight: .5,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: map,
+      center: position,
+      radius: 27.432,
+      draggable: true
+    };
+    circle = new google.maps.Circle(circleOptions);
+  }
+
+  function closeMarkerDisplay() {
+    if (curMarkerDisplay !== null)
+      curMarkerDisplay.close()
+  }
+
+  function makeMarkerCallback(songTag, latLng) {
+    return function() {
+      var max_len = 37;
+      var content = Y.Node.create(
+        Y.Lang.sub(SONG_TAG_TPL, {
+          art_url: songTag.art_url,
+          song: ellipsize(songTag.song, max_len),
+          artist: ellipsize(songTag.artist, max_len),
+          album: ellipsize(songTag.album, max_len),
+          address: songTag.address,
+          stream_url: songTag.stream_url
+        })
+      );
+
+      closeMarkerDisplay();
+      var curMarkerDisplay = new google.maps.InfoWindow({
+        content: content.getDOMNode(),
+        position: latLng,
+        maxWidth: 500,
+      });
+      curMarkerDisplay.open(map);
+      Y.later(300, this, function() {
+          audioPlayer.pause();
+          audioPlayer.setSource(songTag.stream_url);
+          content.one('.playpause').on('click', function(e) {
+            e.preventDefault();
+            audioPlayer.toggle();
+          });
+      });
+      google.maps.event.addListener(curMarkerDisplay, 'closeclick', function() {
+        audioPlayer.pause();
+      });
+    }
+  }
+
   function initMap() {
     var mapOptions = {
       center: WES_COORDS,
       zoom: INIT_ZOOM,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
+      mapTypeId: google.maps.MapTypeId.ROADMAP,
+      disableDoubleClickZoom: true
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
-    Y.on("io:complete", function(id, o, args) {
+    Y.once("io:complete", function(id, o, args) {
       var songTags = Y.JSON.parse(o.responseText);
       for (var i = 0; i < songTags.length; i++) {
         var songTag = songTags[i];
+        var latLng = new google.maps.LatLng(songTag.latitude, songTag.longitude);
         var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(songTag.latitude, songTag.longitude),
-          map: map,
-          title: "a song",
-          animation: google.maps.Animation.DROP,
+            position: latLng,
+            map: map,
+            animation: google.maps.Animation.DROP,
         });
+        google.maps.event.addListener(marker, 'click', makeMarkerCallback(songTag, latLng));
       }
     });
     Y.io("/songs.json");
-    google.maps.event.addListener(map, 'click', function(e) {
+    google.maps.event.addListener(map, 'dblclick', function(e) {
       tagSong(e.latLng, map);
     });
+    google.maps.event.addListener(map, 'click', closeMarkerDisplay);
+    placeRadius(WES_COORDS, map)
   }
 
-  function initAudioPlayer() {
-    // scale = 10000;
-    // var playSlider = new Y.Slider({
-    //   length: '500px',
-    //   min: 0,
-    //   max: scale
-    // });
-    // playSlider.render('#play-slider');
-    // var player = Y.one('#audio-player').getDOMNode();
-    // player.src = "http://previews.7digital.com/clips/480/28905854.clip.mp3";
-    // player.addEventListener('loadedmetadata', function() {
-    //    duration = player.duration;
-    //    // player.addEventListener('timeupdate', function(e) {
-    //    //   curTime = player.currentTime;
-    //    //   playSlider.setValue(Math.floor(scale*curTime/duration));
-    //    // });
-    //    playSlider.on('slideEnd', function(e) {
-    //      // console.log(playSlider.get('value'));
-    //      songTime = playSlider.get('value')/scale * player.duration;
-    //      console.log(songTime);
-    //      player.pause();
-    //      player.currentTime = Math.floor(songTime);
-    //      console.log(player.currentTime);
-    //      player.play();
-    //    });
-    // });
-  }
-
-  Y.on('domready', initAudioPlayer);
   Y.one('window').on('resize', resizeResponse);
   Y.one('window').on('load', initMap);
   resizeResponse();
