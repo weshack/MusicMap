a7301fd093d677e8592e1351d4597ee7c839398b
Matt Adelman <madelman@wesleyan.edu>
2013-05-19 06:53:25 -0400

    Working on users

--- music_map/app/controllers/application_controller.rb
+++ music_map/app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  private
+  helper_method :current_user  
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find(session[:user_id]).name if session[:user_id]
   end
-  helper_method :current_user
 
 end
